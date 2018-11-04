be77c5f8f3a1d25a883143caa69b9baa12718323
Brendan Gregos <bgregos@gmail.com>
2018-11-04 17:56:25 -0500

    named entity now shows all words

--- raw_python/namedEntity.py
+++ raw_python/namedEntity.py
@@ -4,8 +4,19 @@ from collections import Counter
 import en_core_web_sm
 nlp = en_core_web_sm.load()
 
+def xstr(s):
+    return '' if s is None else str(s)
+
 import json
 json = json.loads(open('fixedJSON.json').read())
 for article in json['articles']:
     doc=nlp(article['Sentences'])
-    print([(X.text, X.label_) for X in doc.ents])
+    for X in doc:
+        isEnt = False
+        for y in doc.ents:
+            if X.i >= y.start and X.i < y.end:
+                isEnt = True
+                if X.i == y.start:
+                    print(y.text, y.label_)
+        if not isEnt:
+            print(X.text, X.tag_)
