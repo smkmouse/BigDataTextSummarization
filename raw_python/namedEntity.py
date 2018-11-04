b2dac3b0e24d40d5f4fa636c9ca913865dd77ae9
Brendan Gregos <bgregos@gmail.com>
2018-11-04 16:23:41 -0500

    new raw python named-entity created as a proof of concept

--- raw_python/namedEntity.py
+++ raw_python/namedEntity.py
@@ -6,4 +6,6 @@ nlp = en_core_web_sm.load()
 
 import json
 json = json.loads(open('fixedJSON.json').read())
-
+for article in json['articles']:
+    doc=nlp(article['Sentences'])
+    print([(X.text, X.label_) for X in doc.ents])
